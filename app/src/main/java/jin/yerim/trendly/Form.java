04f6f9f08a5fafc1f256a156a3cbe6cc12d91c69
xxyyrrr <yrjin241004@gmail.com>
2023-02-26 21:42:20 -0500

    commit change

--- app/src/main/java/jin/yerim/trendly/Form.java
+++ app/src/main/java/jin/yerim/trendly/Form.java
@@ -1,29 +1,93 @@
 package jin.yerim.trendly;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class Form extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form);
+        ListView listview = findViewById(R.id.listView);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         FloatingActionButton btn = findViewById(R.id.add_circle1);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent it = new Intent(Form.this, Formsubmit.class);
-                startActivity(it);
+                ListViewAdapter adapter = new ListViewAdapter();
+
+                db.collection("charity")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData().get("charity") + document.getData().get("clothesnum") + document.getData().get("quality"));
+                                        // Create a reference with an initial file path and name
+                                    }
+                                }
+                                listview.setAdapter(adapter);
+                            }
+                        });
             }
         });
+        Intent it = new Intent(Form.this, Formsubmit.class);
+        startActivity(it);
+    }
+
+    public class ListViewAdapter extends BaseAdapter {
+        ArrayList<Item> items = new ArrayList<Item>();
+
+        public void addItem(Item item) {
+            items.add(item);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            final Context context = viewGroup.getContext();
+            final Item Item = items.get(position);
+            return convertView;
+        }
     }
 }
